efd8706e6ad9161ef119b523e130b719c0071906
Aeon1 <joel.r@live.com.mx>
2016-12-16 20:56:13 -0700

    cambios

---
+++ Turnomatico/platforms/browser/www/plugins/com.iigservices.cordova.plugin/www/SignaturePrinter.js
@@ -0,0 +1,17 @@
+cordova.define("com.iigservices.cordova.plugin.SignaturePrinter", function(require, exports, module) { window.initPrinter = function(printer, callback) {
+    cordova.exec(callback, function(err) {
+        console.log("Error while initPrinter: "+err);
+    }, "SignaturePrinter", "initPrinter", [printer]);
+};
+window.printSignature = function(image,width,height, callback) {
+    cordova.exec(callback, function(err) {
+        console.log("Error while printSignature: "+err);
+    }, "SignaturePrinter", "printSignature", [image,width,height]);
+};
+window.printText = function(message, callback) {
+    cordova.exec(callback, function(err) {
+        console.log("Error while printText: "+err);
+    }, "SignaturePrinter", "printText", [message]);
+};
+
+});

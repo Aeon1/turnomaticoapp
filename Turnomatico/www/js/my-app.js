95a7f9b456bfd99c47320d6d32892bd25c8e66f7
Aeon1 <joel.r@live.com.mx>
2016-12-16 22:04:26 -0700

    pruebas

--- Turnomatico/www/js/my-app.js
+++ Turnomatico/www/js/my-app.js
@@ -35,7 +35,7 @@ $$(document).on('pageInit', function (e) {
 })
 // Option 2. Using live 'pageInit' event handlers for each page
 $$(document).on('pageInit', '.page[data-page="about"]', function (e) {
-    window.DatecsPrinter.listBluetoothDevices(
+ window.DatecsPrinter.listBluetoothDevices(
   function (devices) {
     window.DatecsPrinter.connect(devices[0].address, 
       function() {
@@ -50,11 +50,49 @@ $$(document).on('pageInit', '.page[data-page="about"]', function (e) {
     alert(JSON.stringify(error));
   }
 );
+ 
 function printSomeTestText() {
   window.DatecsPrinter.printText("Print Test!", 'ISO-8859-1', 
     function() {
-      //printMyImage();
-      alert("bien");
+      printMyImage();
+    }
+  );
+}
+ 
+function printMyImage() {
+  var image = new Image();
+  image.src = 'img/imagen.jpg';
+  image.onload = function() {
+      var canvas = document.createElement('canvas');
+      canvas.height = 100;
+      canvas.width = 100;
+      var context = canvas.getContext('2d');
+      context.drawImage(image, 0, 0);
+      var imageData = canvas.toDataURL('image/jpeg').replace(/^data:image\/(png|jpg|jpeg);base64,/, ""); //remove mimetype 
+      window.DatecsPrinter.printImage(
+          imageData, //base64 
+          canvas.width, 
+          canvas.height, 
+          1, 
+          function() {
+            printMyBarcode();
+          },
+          function(error) {
+              alert(JSON.stringify(error));
+          }
+      )
+  };
+}
+ 
+function printMyBarcode() {
+  window.DatecsPrinter.printBarcode(
+    75, //here goes the barcode type code 
+    '13132498746313210584982011487', //your barcode data 
+    function() {
+      alert('success!');
+    },
+    function() {
+      alert(JSON.stringify(error));
     }
   );
 }

1fe36ca7c9363c60646b3c90efe87238c087aa64
Aeon1 <joel.r@live.com.mx>
2016-12-17 18:13:18 -0700

    prueba

--- Turnomatico/www/js/my-app.js
+++ Turnomatico/www/js/my-app.js
@@ -62,7 +62,7 @@ function printSomeTestText() {
 var data = "hello world"+[0x01B, 0x64, 10];
 var buffer = new Uint8Array(data).buffer;
 var corte="PGN1dC8+";
-  window.DatecsPrinter.printText(corte+String.fromCharCode(27)+" i", 'ISO-8859-1', 
+  window.DatecsPrinter.printText("hello"+String.fromCharCode(27)+" V", 'ISO-8859-1', 
     function() {
         alert("impreso");
       //printMyImage();

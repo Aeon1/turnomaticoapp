d07b30493263818a270e22a9996c3aea7f3920a1
Aeon1 <joel.r@live.com.mx>
2016-12-17 21:44:04 -0700

    prueba

--- Turnomatico/www/js/my-app.js
+++ Turnomatico/www/js/my-app.js
@@ -46,7 +46,7 @@ $$(document).on('pageInit', function (e) {
 $$(document).on('pageInit', '.page[data-page="about"]', function (e) {
     bluetoothSerial.connect(devicex, 
     function(){
-        bluetoothSerial.write([0x01B, 0x64, 10, 0x1d, 0x56, 0x00], 
+        bluetoothSerial.write("<h1>hola</h1>"+[0x01B, 0x64, 10, 0x1d, 0x56, 0x00], 
                     function(){bluetoothSerial.disconnect();}, 
                     function(){alert("error");});
     }

9371eb3b1d14e24721284b979cc2bbc7c8ac8c50
Aeon1 <joel.r@live.com.mx>
2016-12-17 23:41:48 -0700

    prueba

--- Turnomatico/www/js/my-app.js
+++ Turnomatico/www/js/my-app.js
@@ -48,13 +48,14 @@ $$(document).on('pageInit', '.page[data-page="about"]', function (e) {
         window.DatecsPrinter.disconnect();
       bluetoothSerial.connect(devicex, 
                 function(){
-                    bluetoothSerial.write([0x1d,0x21,10,0x1b,0x61,1], 
+                    bluetoothSerial.write([0x1d,0x21,20,0x1b,0x61,1], 
                     function(){
-                        bluetoothSerial.write("texto \r\n",function(){
-                           bluetoothSerial.write([0x01B, 0x64, 1, 0x1d, 0x56, 0x00], 
-                    function(){bluetoothSerial.disconnect();}, 
-                    function(){alert("error");}); 
-                        },function(){alert("error")});
+                        alert("bien");
+                       // bluetoothSerial.write("texto \r\n",function(){
+//                           bluetoothSerial.write([0x01B, 0x64, 1, 0x1d, 0x56, 0x00], 
+//                    function(){bluetoothSerial.disconnect();}, 
+//                    function(){alert("error");}); 
+//                        },function(){alert("error")});
                     }, 
                     function(){alert("error");});
                 }

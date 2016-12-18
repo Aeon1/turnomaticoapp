db864a56706b67f3bd11e31c3e1e142dba383cab
Aeon1 <joel.r@live.com.mx>
2016-12-17 18:39:01 -0700

    pruebas

--- Turnomatico/www/js/my-app.js
+++ Turnomatico/www/js/my-app.js
@@ -39,71 +39,22 @@ $$(document).on('pageInit', function (e) {
 })
 // Option 2. Using live 'pageInit' event handlers for each page
 $$(document).on('pageInit', '.page[data-page="about"]', function (e) {
- window.DatecsPrinter.listBluetoothDevices(
-  function (devices) {
-    window.DatecsPrinter.connect(devices[0].address, 
-      function() {
-        printSomeTestText();
-         var builder = new epson.ePOSBuilder();
-         builder.addCut(builder.CUT_FEED);
-
-      },
-      function() {
-        alert(JSON.stringify(error));
-      }
-    );
-  },
-  function (error) {
-    alert(JSON.stringify(error));
-  }
-);
- 
-function printSomeTestText() {
-  window.DatecsPrinter.printText("hello", 'ISO-8859-1', 
-    function() {
-        alert("impreso");
-      //printMyImage();
-    }
-  );
-}
- 
-function printMyImage() {
-  var image = new Image();
-  image.src = 'img/imagen.jpg';
-  image.onload = function() {
-      var canvas = document.createElement('canvas');
-      canvas.height = 100;
-      canvas.width = 100;
-      var context = canvas.getContext('2d');
-      context.drawImage(image, 0, 0);
-      var imageData = canvas.toDataURL('image/jpeg').replace(/^data:image\/(png|jpg|jpeg);base64,/, ""); //remove mimetype 
-      
-      window.DatecsPrinter.printImage(
-          imageData, //base64 
-          canvas.width, 
-          canvas.height, 
-          1, 
-          function() {
-            printMyBarcode();
-          },
-          function(error) {
-              alert(JSON.stringify(error));
-          }
-      )
-  };
-}
- 
-function printMyBarcode() {
-  window.DatecsPrinter.printBarcode(
-    75, //here goes the barcode type code 
-    '13132498746313210584982011487', //your barcode data 
-    function() {
-      alert('success!');
-    },
-    function() {
-      alert(JSON.stringify(error));
-    }
-  );
-}
+ ZJPrinter.list(function(data){
+        ZJPrinter.connect(function(data){
+            ZJPrinter.printText(function(data){
+                console.log("Success");
+                alert(data)
+            },function(err){
+                console.log("Error");
+                alert(err)
+            }, "String to Print")
+        },function(err){
+            console.log("Error");
+            console.log(err)
+        }, "PrinterName")
+    },function(err){
+        console.log("Error");
+        console.log(err);
+    })
 
 })

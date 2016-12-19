384edfded7722a7a99f59c3f0f4a2b5bd3b50133
Aeon1 <joel.r@live.com.mx>
2016-12-19 11:44:47 -0700

    prueba

--- Turnomatico/www/js/my-app.js
+++ Turnomatico/www/js/my-app.js
@@ -48,7 +48,7 @@ $$(document).on('pageInit', '.page[data-page="aboutx"]', function (e) {
         window.DatecsPrinter.disconnect();
       bluetoothSerial.connect(devicex, 
                 function(){
-                    bluetoothSerial.write([0x1d,0x21,8,8,0x1b,0x61,1,0x01B,0x2d,0], 
+                    bluetoothSerial.write([0x1d, 0x21, 0x48 ,0x1d,0x21,2,0x1b,0x61,1], 
                     function(){  
                         bluetoothSerial.write(" Audios \r\n",function(){
                            bluetoothSerial.write([0x01B, 0x64, 3, 0x1d, 0x56, 0x00], 

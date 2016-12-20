4eaf3c7e037150f497d4594fbd3ae19eba11a7ed
Aeon1 <joel.r@live.com.mx>
2016-12-20 13:32:47 -0700

    prueba

--- Turnomatico/www/js/my-app.js
+++ Turnomatico/www/js/my-app.js
@@ -93,19 +93,19 @@ function imprimir(){
     bluetoothSerial.connect(devices[0].address, 
                 function(){
                     bluetoothSerial.write([0x1c,0x28,0x45,6,0,62,2,0x21,0x48,49,5]);
-                   // bluetoothSerial.write([0x1b,0x21,0,0x1b,0x61,1,0x1d,0x21,2]);
-//                    bluetoothSerial.write("Gobierno del Estado de Sinaloa\r\n");
-//                    bluetoothSerial.write([0x1d,0x21,1]);
-//                    bluetoothSerial.write("USE\r\n\n");
-//                    bluetoothSerial.write([0x1d,0x21,0]);
-//                    bluetoothSerial.write("Numero de turno:\r\n\n");
-//                    bluetoothSerial.write([0x1d,0x21,2]);
-//                    bluetoothSerial.write(turno+"\r\n\n");
-//                    bluetoothSerial.write([0x1d,0x21,0]);
-//                    bluetoothSerial.write("Servicio:"+servicio+"\r\n\n\n");
-//                    bluetoothSerial.write([0x1d,0x21,0]);
-//                    bluetoothSerial.write("Hora:"+hora+"\r\n");
-//                    bluetoothSerial.write("Fecha:"+fecha+"\r\n");
+                    bluetoothSerial.write([0x1b,0x21,0,0x1b,0x61,1,0x1d,0x21,2]);
+                    bluetoothSerial.write("Gobierno del Estado de Sinaloa\r\n");
+                    bluetoothSerial.write([0x1d,0x21,1]);
+                    bluetoothSerial.write("USE\r\n\n");
+                    bluetoothSerial.write([0x1d,0x21,0]);
+                    bluetoothSerial.write("Numero de turno:\r\n\n");
+                    bluetoothSerial.write([0x1d,0x21,2]);
+                    bluetoothSerial.write(turno+"\r\n\n");
+                    bluetoothSerial.write([0x1d,0x21,0]);
+                    bluetoothSerial.write("Servicio:"+servicio+"\r\n\n\n");
+                    bluetoothSerial.write([0x1d,0x21,0]);
+                    bluetoothSerial.write("Hora:"+hora+"\r\n");
+                    bluetoothSerial.write("Fecha:"+fecha+"\r\n");
                     bluetoothSerial.write([0x01B, 0x64, 5, 0x1d, 0x56, 0x00],
                     function(){
                         bluetoothSerial.disconnect(function(){},function(){});

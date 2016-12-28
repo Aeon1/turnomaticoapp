c62bd101a6606c40a1e6d29f066554397952e90d
Aeon1 <joel.r@live.com.mx>
2016-12-28 16:50:53 -0700

    prueba de socket

--- Turnomatico/www/js/my-app.js
+++ Turnomatico/www/js/my-app.js
@@ -13,17 +13,28 @@ var mainView = myApp.addView('.view-main', {
 var devicex="";
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {    
-    //window.DatecsPrinter.listBluetoothDevices(
-//  function (devices) {
-//    devicex=devices[0].address;
-//  },
-//  function (error) {
-//    alert(JSON.stringify(error));
-//  }
-//);
+var ws = new WebSocket('http://192.168.1.69:3000/');
+
+    ws.onopen = function () {
+        alert('open');
+        this.send('hello');
+    };
+
+    ws.onmessage = function (event) {
+        alert(event.data);
+        this.close();
+    };
+
+    ws.onerror = function () {
+        alert('error occurred!');
+    };
+
+    ws.onclose = function (event) {
+        alert('close code=' + event.code);
+    };
 
      
-});
+},false);
 
 
 // Now we need to run the code that will be executed only for About page.
@@ -44,21 +55,7 @@ $$(document).on('pageInit', function (e) {
 })
 // Option 2. Using live 'pageInit' event handlers for each page
 $$(document).on('pageInit', '.page[data-page="aboutx"]', function (e) {
-//centrado= 0x1b,0x61,1
-    //    window.DatecsPrinter.disconnect();
-//      bluetoothSerial.connect(devicex, 
-//                function(){
-//                    bluetoothSerial.write([0x1c , 0x21, 0x48 ,0x1d,0x21,2,0x1b,0x61,1], 
-//                    function(){  
-//                        bluetoothSerial.write("Audios \r\n",function(){
-//                           bluetoothSerial.write([0x01B, 0x64, 3, 0x1d, 0x56, 0x00], 
-//                    function(){bluetoothSerial.disconnect();}, 
-//                    function(){alert("error");}); 
-//                        },function(){alert("error")});
-//                    }, 
-//                    function(){alert("error");});
-//                }
-//            , function(){alert("fallo la conexcion");});
+
 })
 function pressbtn(num){
     var actual=$$("input#numero_celular").val();
@@ -92,19 +89,19 @@ function imprimir(){
   function (devices) {
     bluetoothSerial.connect(devices[0].address, 
                 function(){
-                    bluetoothSerial.write([0x1b,0x21,0,0x1b,0x61,1,0x1d,0x21,32,0x1d,0x21,112]);
-                    //bluetoothSerial.write("Gobierno del Estado de Sinaloa\r\n");
-//                    bluetoothSerial.write([0x1d,0x21,1]);
+                    bluetoothSerial.write([0x1b,0x21,0,0x1b,0x61,1,0x1d,0x21,2]);
+                    bluetoothSerial.write("Gobierno del Estado de Sinaloa\r\n");
+                    bluetoothSerial.write([0x1d,0x21,1]);
                     bluetoothSerial.write("USE\r\n\n");
-//                    bluetoothSerial.write([0x1d,0x21,0]);
-//                    bluetoothSerial.write("Numero de turno:\r\n\n");
-//                    bluetoothSerial.write([0x1d,0x21,2]);
-//                    bluetoothSerial.write(turno+"\r\n\n");
-//                    bluetoothSerial.write([0x1d,0x21,0]);
-//                    bluetoothSerial.write("Servicio:"+servicio+"\r\n\n\n");
-//                    bluetoothSerial.write([0x1d,0x21,0]);
-//                    bluetoothSerial.write("Hora:"+hora+"\r\n");
-//                    bluetoothSerial.write("Fecha:"+fecha+"\r\n");
+                    bluetoothSerial.write([0x1d,0x21,0]);
+                    bluetoothSerial.write("Numero de turno:\r\n\n");
+                    bluetoothSerial.write([0x1d,0x21,2]);
+                    bluetoothSerial.write(turno+"\r\n\n");
+                    bluetoothSerial.write([0x1d,0x21,0]);
+                    bluetoothSerial.write("Servicio:"+servicio+"\r\n\n\n");
+                    bluetoothSerial.write([0x1d,0x21,0]);
+                    bluetoothSerial.write("Hora:"+hora+"\r\n");
+                    bluetoothSerial.write("Fecha:"+fecha+"\r\n");
 //                    bluetoothSerial.write([0x1c,0x28,0x45,4,0,65,2,48,49]);//deshabilita la impresion de imagen
                     bluetoothSerial.write([0x01B, 0x64, 5, 0x1d, 0x56, 0x00],
                     function(){

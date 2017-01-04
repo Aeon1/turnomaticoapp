a0490087bf7f95ddd67051055c39091d2059df53
Aeon1 <joel.r@live.com.mx>
2017-01-04 14:38:08 -0700

    prueba de bufer

--- Turnomatico/platforms/browser/www/js/my-app.js
+++ Turnomatico/platforms/browser/www/js/my-app.js
@@ -206,6 +206,7 @@ function imprimir(key,fecha,hora){
                     bluetoothSerial.write("Fecha:"+fecha+"\r\n");
                     bluetoothSerial.write([0x01B, 0x64, 5, 0x1d, 0x56, 0x00],
                     function(){
+                        bluetoothSerial.write([0x10,0x14,8,1,3,14,1,6,2,8]);
                         bluetoothSerial.disconnect(function(){
                           myApp.modal({
                             title:  '<img src="img/checked.svg" style="height:50px;margin:auto;display:block"/>'+
@@ -229,26 +230,26 @@ function imprimir(key,fecha,hora){
                             ]
                           })
                         },function(){});
-                        window.DatecsPrinter.listBluetoothDevices(
-                          function (devices) {
-                            bluetoothSerial.connect(devices[0].address, 
-                            function() {
-                                    bluetoothSerial.write([0x01B, 0x64, 5, 0x1d, 0x56, 0x00],
-                                    function(){
-                                        bluetoothSerial.write([0x10,0x14,8,1,3,14,1,6,2,8],function(){alert("buffer vaciado");},function(error){alert("buffer error: "+error);});
-                                        bluetoothSerial.disconnect();
-                                    },function(error){
-                                        myApp.alert(error,"Error");
-                                    });
-                              },
-                              function() {
-                                alert(JSON.stringify(error));
-                              }); 
-                          },
-                          function (error) {
-                            alert(JSON.stringify(error));
-                          }
-                        );
+                        //window.DatecsPrinter.listBluetoothDevices(
+//                          function (devices) {
+//                            bluetoothSerial.connect(devices[0].address, 
+//                            function() {
+//                                    bluetoothSerial.write([0x01B, 0x64, 5, 0x1d, 0x56, 0x00],
+//                                    function(){
+//                                        bluetoothSerial.write([0x10,0x14,8,1,3,14,1,6,2,8],function(){alert("buffer vaciado");},function(error){alert("buffer error: "+error);});
+//                                        bluetoothSerial.disconnect();
+//                                    },function(error){
+//                                        myApp.alert(error,"Error");
+//                                    });
+//                              },
+//                              function() {
+//                                alert(JSON.stringify(error));
+//                              }); 
+//                          },
+//                          function (error) {
+//                            alert(JSON.stringify(error));
+//                          }
+//                        );
                     },
                     function(error){
                         myApp.alert(error,"Error");
